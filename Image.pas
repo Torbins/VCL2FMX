59f423c8296d5108ea4df46c07143f834491b36a
Mykola Petrivskyi <mikolatorbins@gmail.com>
2024-10-10 18:17:33 +0300

    Revorked image data convertor code

--- Image.pas
+++ Image.pas
@@ -8,127 +8,85 @@ unit Image;
 
 interface
 
+function ProcessImage(sData, APad: String): String;
+
+implementation
+
 uses
-  Vcl.Graphics,
-  FMX.Objects;
+  System.Classes, System.SysUtils, Vcl.Graphics, Vcl.Imaging.Jpeg, Vcl.Imaging.GIFImg, Vcl.Imaging.PngImage, PatchLib;
 
 type
   TGraphicAccess = class(Vcl.Graphics.TGraphic)
   end;
 
 function ProcessImage(sData, APad: String): String;
-
-function ImageDFMtoFMX(sData: String): String;
-
-function ImageToHex(Image:FMX.Objects.TImage; LineLen:integer): String;
-
-implementation
-
-uses
-  System.Classes,
-  Vcl.ExtCtrls,
-  System.SysUtils,
-  Vcl.Imaging.Jpeg,
-  Vcl.Imaging.GIFImg,
-  Vcl.Imaging.PngImage;
-
+const
+  LineLen = 64;
 var
-  FPad: String;
-
-function ProcessImage(sData, APad: String): String;
+  InStr, OutStr, LineStr: String;
+  InStream, OutStream: TMemoryStream;
+  GraphClassName: ShortString;
+  Graphic: TGraphic;
+  Pic: TPngImage;
+  LineNum, DataStart, DataLen: Integer;
 begin
-  FPad := APad;
-  Result :=    APad +'  MultiResBitmap = <'+
-  sLineBreak + APad +'    item '+
-  sLineBreak + APad +'      PNG = { '+ ImageDFMtoFMX(sData) +'} '+
-  sLineBreak + APad +'    end>';
-end;
+  if sData.StartsWith('{') then
+    DataStart := 2
+  else
+    DataStart := 1;
+  DataLen := Length(sData) - DataStart + 1;
+  if sData.EndsWith('}') then
+    Dec(DataLen);
 
-function ImageDFMtoFMX(sData: String): String;
-var
-  Linput: String;
-  Loutput: TMemoryStream;
-  LclsName: ShortString;
-  Lgraphic: TGraphic;
-  img2: Vcl.ExtCtrls.TImage;
-  img1: FMX.Objects.TImage;
-  stream: TMemoryStream;
-begin
-  // Remove caracteres
-  sData := StringReplace(sData, '{', EmptyStr, []);
-  sData := StringReplace(sData, '}', EmptyStr, []);
-
-  // Inicializa
-  Linput  := sData;
-  Loutput := TMemoryStream.Create;
+  InStr := Copy(sData, DataStart, DataLen);
+  Graphic := nil;
+  Pic := nil;
+  InStream := TMemoryStream.Create;
+  OutStream := TMemoryStream.Create;
   try
-    // Carrega dados para memoria
-    Loutput.Size := Length(Linput) div 2;
-    HexToBin(PChar(Linput), Loutput.Memory^, Loutput.Size);
-    LclsName := PShortString(Loutput.Memory)^;
+    InStream.Size := Length(InStr) div 2;
+    HexToBin(PChar(InStr), InStream.Memory^, InStream.Size);
+    GraphClassName := PShortString(InStream.Memory)^;
 
-    // Cria imagem FMX
-    img1 := FMX.Objects.TImage.Create(nil);
-    // Cria imagem VCL
-    Lgraphic := TGraphicClass(FindClass(UTF8ToString(LclsName))).Create;
-    img2 := nil;
-    try
-      // Carrega dados para imagem VCL
-      Loutput.Position := 1 + Length(LclsName);
-      TGraphicAccess(Lgraphic).ReadData(Loutput);
-      img2 := TImage.Create(nil);
-      if not (Lgraphic is TMetafile) then
-        img2.Picture.Assign(Lgraphic)
-      else
-      begin
-        var b := img2.Picture.Bitmap; // Create empty bitmap
-        b.Canvas.Brush.Color := clBlack;
-        b.SetSize(Lgraphic.Width, Lgraphic.Height);
-        b.Canvas.Draw(0, 0, Lgraphic);
-      end;
+    Graphic := TGraphicClass(FindClass(UTF8ToString(GraphClassName))).Create;
+    InStream.Position := 1 + Length(GraphClassName);
+    TGraphicAccess(Graphic).ReadData(InStream);
 
-      // Converte de VCL para FMX
-      stream:= TMemoryStream.Create;
-      try
-        img2.Picture.SaveToStream(stream);
-        stream.Position := 0;
-        img1.Bitmap.LoadFromStream(stream);
-      finally
-        stream.Free;
-      end;
-
-      // Retorna imagem convertida de FMX para texto
-      Result := ImageToHex(img1, 64);
-    finally
-      img1.Free;
-      img2.Free;
-      Lgraphic.Free;
+    if (Graphic is TPngImage) or (Graphic is TBitmap) or (Graphic is TWICImage) then
+    begin
+      Pic := TPngImage.Create;
+      Pic.Assign(Graphic);
+    end
+    else
+    begin
+      Pic := TPngImage.CreateBlank(COLOR_RGB, 8, Graphic.Width, Graphic.Height);
+      Pic.Canvas.Draw(0, 0, Graphic, 255);
     end;
-  finally
-    Loutput.Free;
-  end;
-end;
 
-function ImageToHex(Image:FMX.Objects.TImage; LineLen:integer): String;
-var
-  ms: TMemoryStream;
-  s, t: String;
-  LineNum: Integer;
-begin
-  ms := TMemoryStream.Create;
-  try
-    image.Bitmap.SaveToStream(ms);
-    SetLength(t, ms.Size * 2);
-    BinToHex(ms.Memory^, PChar(t), ms.Size);
+    Pic.SaveToStream(OutStream);
+    OutStream.Position := 0;
+    SetLength(OutStr, OutStream.Size * 2);
+    BinToHex(OutStream.Memory^, PChar(OutStr), OutStream.Size);
+
     LineNum := 0;
     repeat
-      s := Copy(t, LineLen * LineNum + 1, LineLen);
-      if s <> '' then
-        Result := Result + sLineBreak + FPad + '        ' + s;
+      LineStr := Copy(OutStr, LineLen * LineNum + 1, LineLen);
+      if LineStr <> '' then
+        Result := Result + CRLF + APad + '        ' + LineStr;
       Inc(LineNum);
-    until Length(s) < LineLen;
+    until Length(LineStr) < LineLen;
+
+    Result := 'MultiResBitmap = <' +
+      CRLF + APad + '    item ' +
+      CRLF + APad + '      Width = ' + Graphic.Width.ToString +
+      CRLF + APad + '      Height = ' + Graphic.Height.ToString +
+      CRLF + APad + '      PNG = {' + Result + '}' +
+      CRLF + APad + '    end>';
   finally
-    ms.Free;
+    OutStream.Free;
+    InStream.Free;
+    Pic.Free;
+    Graphic.Free;
   end;
 end;
 
